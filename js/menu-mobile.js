8743fa8f0b26e8771c5de38e85a11b145b40d486
Andy <142624745+idelcarlosribeiro@users.noreply.github.com>
2024-03-25 22:12:34 -0300

    .

--- js/menu-mobile.js
+++ js/menu-mobile.js
@@ -1,7 +1,33 @@
-function clickMenu() {
-  if (itens.style.display == 'block') {
-      itens.style.display = 'none'
-  } else {
-    itens.style.display = 'block'
+var menuMob = document.getElementById("menu-mobile");
+var content = document.getElementById("tudo");
+var menuBur = document.getElementById("menu-burg");
+var topo = document.getElementById('texto')
+var siderbar = false;
+
+function fechar(){
+  siderbar = !siderbar;
+  if (siderbar){
+    menuMob.style.marginLeft = '-0vw';
+    menuMob.style.animationName = "showSidebar";
+    menuBur.style.display = 'none'
+    content.style.filter = 'blur(3px)';
+    topo.style.filter = "blur(3px)";
+    
+  }else{
+    menuMob.style.marginLeft = "-100vw";
+    menuBur.style.display = "block";
+    menuMob.style.animationName = "";
+    content.style.filter = "";
+    topo.style.filter = "";
   }
 }
+function closeSidebar() {
+  if(siderbar){
+    fechar();
+  }
+}
+window.addEventListener('resize', function(event){
+  if (window.innerWidth > 604 && siderbar){
+    fechar();
+  }
+});
