306b385b126196fc04739ba97f18f51852670954
Andy <142624745+idelcarlosribeiro@users.noreply.github.com>
2024-04-11 19:29:52 -0300

    .

--- js/dark.js
+++ js/dark.js
@@ -1,3 +1,5 @@
+
+/*efeito aparecer*/
 const myObserver = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if(entry.isIntersecting){
@@ -11,3 +13,23 @@ const myObserver = new IntersectionObserver((entries) => {
 const elements = document.querySelectorAll(".hidden");
 
 elements.forEach((element) => myObserver.observe(element))
+/*efeito aparecer FIM*/
+
+
+
+/* Carrosel animado */
+let next = document.querySelector('.next');
+let prev = document.querySelector('.prev');
+
+next.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item');
+    document.querySelector('.slide').appendChild(items[0]);
+})
+
+prev.addEventListener('click', function(){
+    let items = document.querySelectorAll('.item')
+    document.querySelector('.slide').prepend(items[items.length - 1])
+})
+
+
+/* Carrosel animado FIM*/
