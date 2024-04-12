ba680ef3593e5845d06dfd214973faf74b4a17e7
Andy <142624745+idelcarlosribeiro@users.noreply.github.com>
2024-04-12 15:12:53 -0300

    .

--- js/dark.js
+++ js/dark.js
@@ -18,18 +18,57 @@ elements.forEach((element) => myObserver.observe(element))
 
 
 /* Carrosel animado */
-let next = document.querySelector('.next');
-let prev = document.querySelector('.prev');
+let items = document.querySelectorAll(".slider .list .item");
+let next = document.getElementById("next");
+let prev = document.getElementById("prev");
+let thumbnails = document.querySelectorAll(".thumbnail .item");
 
-next.addEventListener('click', function(){
-    let items = document.querySelectorAll('.item');
-    document.querySelector('.slide').appendChild(items[0]);
-})
+// config param
+let countItem = items.length;
+let itemActive = 0;
+// event next click
+next.onclick = function () {
+  itemActive = itemActive + 1;
+  if (itemActive >= countItem) {
+    itemActive = 0;
+  }
+  showSlider();
+};
+//event prev click
+prev.onclick = function () {
+  itemActive = itemActive - 1;
+  if (itemActive < 0) {
+    itemActive = countItem - 1;
+  }
+  showSlider();
+};
+// auto run slider
+let refreshInterval = setInterval(() => {
+  next.click();
+}, 10000);
+function showSlider() {
+  // remove item active old
+  let itemActiveOld = document.querySelector(".slider .list .item.active");
+  let thumbnailActiveOld = document.querySelector(".thumbnail .item.active");
+  itemActiveOld.classList.remove("active");
+  thumbnailActiveOld.classList.remove("active");
 
-prev.addEventListener('click', function(){
-    let items = document.querySelectorAll('.item')
-    document.querySelector('.slide').prepend(items[items.length - 1])
-})
+  // active new item
+  items[itemActive].classList.add("active");
+  thumbnails[itemActive].classList.add("active");
 
+  // clear auto time run slider
+  clearInterval(refreshInterval);
+  refreshInterval = setInterval(() => {
+    next.click();
+  }, 10000);
+}
 
+// click thumbnail
+thumbnails.forEach((thumbnail, index) => {
+  thumbnail.addEventListener("click", () => {
+    itemActive = index;
+    showSlider();
+  });
+});
 /* Carrosel animado FIM*/
